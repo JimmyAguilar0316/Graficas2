8501a7a5207c5d46f14868bb56d6ea7071d1dd47
JimmyAguilar03 <Jaime.atrejo03@gmail.com>
2024-04-25 22:17:58 -0600

    graficas

---
+++ hola.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+st.title('Aplicación de gráficas')
+
+
+st.header('Demo App')
+
+st.write('Demo de sitio con gráficas')
+
+titanic_link='titanic(1).csv'
+titanic_data=pd.read_csv(titanic_link)
+st.dataframe(titanic_data)
+
+#fig,ax=plt.subplots()
+#ax.hist(titanic_data['fare'])
+#st.header('Histograma del Titanic')
+#st.pyplot(fig)
